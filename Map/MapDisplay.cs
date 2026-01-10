318aa8dadc8f1b9be42518b63a18ddd629e1439d
Emerson <110031930+Fr-Emerson@users.noreply.github.com>
2026-01-09 22:23:11 -0300

    Adicionar métodos para gerar e exibir mapas de ruído

--- Map/MapDisplay.cs
+++ Map/MapDisplay.cs
@@ -4,6 +4,28 @@ namespace Map
 {
     public class MapDisplay : MonoBehaviour
     {
-        
+        public Renderer textureRenderer;
+
+        public void DrawnNoiseMap(float[,] noiseMap)
+        {
+            int width = noiseMap.GetLength(0);
+            int height = noiseMap.GetLength(1);
+            
+            Texture2D texture = new Texture2D(width, height);
+            
+            Color[] colors = new Color[width * height];
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    colors[y * width + x] = Color.Lerp(Color.black, Color.white, noiseMap[x, y]);
+                }
+            }
+            texture.SetPixels(colors);
+            texture.Apply();
+            
+            textureRenderer.sharedMaterial.mainTexture = texture;
+            textureRenderer.transform.localScale = new Vector3(width, 1, height);
+        }
     }
 }
--- Map/MapGenerator.cs
+++ Map/MapGenerator.cs
@@ -4,6 +4,17 @@ namespace Map
 {
     public class MapGenerator : MonoBehaviour
     {
-        
+        public int mapWidth;
+        public int mapHeight;
+        public float noiseScale;
+
+        public bool autoUpdate;
+        public void GenerateMap()
+        {
+             float[,] noiseMap =  Noise.GeneratedNoiseMap(mapWidth, mapHeight, noiseScale);
+
+             MapDisplay display = FindFirstObjectByType<MapDisplay>();
+              display.DrawnNoiseMap(noiseMap);
+        }
     }
 }

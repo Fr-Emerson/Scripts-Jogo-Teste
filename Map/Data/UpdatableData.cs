44962c17fb2bf7d5000c7abcc41c9cde5f61cdef
Emerson <110031930+Fr-Emerson@users.noreply.github.com>
2026-01-12 22:47:50 -0300

    Adicionar classes ScriptableObject para NoiseData, TerrainData, TextureData e UpdatableData; incluir arquivos .meta correspondentes.

---
+++ Map/Data/UpdatableData.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Map.Data
+{
+    [CreateAssetMenu(fileName = "FILENAME", menuName = "MENUNAME", order = 0)]
+    public class UpdatableData : ScriptableObject
+    {
+        
+    }
+}
